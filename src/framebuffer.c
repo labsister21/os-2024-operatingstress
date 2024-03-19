74d8c75e4f0905a3abcd3dc6a023911e61e840a2
TazakiN <tazkia.nizami@gmail.com>
2024-03-19 13:32:38 +0700

    fix: green bg

--- src/framebuffer.c
+++ src/framebuffer.c
@@ -7,11 +7,11 @@
 
 void framebuffer_set_cursor(uint8_t r, uint8_t c)
 {
-    out(0x3D4, 0x0A);
-    out(0x3D5, (in(0x3D5) & 0xC0) | r);
+    out(CURSOR_PORT_CMD, 0x0A);
+    out(CURSOR_PORT_DATA, (in(CURSOR_PORT_DATA) & 0xC0) | r);
 
-    out(0x3D4, 0x0B);
-    out(0x3D5, (in(0x3D5) & 0xE0) | c);
+    out(CURSOR_PORT_CMD, 0x0B);
+    out(CURSOR_PORT_DATA, (in(CURSOR_PORT_DATA) & 0xE0) | c);
 }
 
 void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg)
@@ -24,8 +24,12 @@ void framebuffer_write(uint8_t row, uint8_t col, char c, uint8_t fg, uint8_t bg)
 
 void framebuffer_clear(void)
 {
-    uint16_t attrib = 0x0000;
+    uint16_t attrib = 0x0F00;
     volatile uint16_t *where = (volatile uint16_t *)0xB8000;
-
     memset((void *)where, ' ' | (attrib << 8), 80 * 25 * 2);
+
+    for (int i = 1; i < 80 * 25 * 2; i += 2)
+    {
+        FRAMEBUFFER_MEMORY_OFFSET[i] = 0x00;
+    }
 }
