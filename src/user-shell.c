df927900f0ef36504f5f0b20bd41db5bae15536a
TazakiN <tazkia.nizami@gmail.com>
2024-05-18 21:37:48 +0700

    feat: cat command

--- src/user-shell.c
+++ src/user-shell.c
@@ -36,16 +36,21 @@ void splash()
     printStr("                              |     | ______| |_____|\n\n", BIOS_RED);
 }
 
+uint32_t id = 0;
 uint32_t depth = 0;
 uint32_t listCluster[100];
-char* listDir[100];
-struct ClusterBuffer cl= {0};
-void parseCommand(uint32_t command){
+char *listDir[100];
+struct ClusterBuffer cl = {0};
+void parseCommand(uint32_t command)
+{
     // cd
-    if(memcmp((char*) command, "cd", 2)==0){ // change directory
-        printStr((char *) command, BIOS_LIGHT_BLUE);
-        if (memcmp("..", (void *) command + 3, 2) == 0){
-            if (depth == 0){
+    if (memcmp((char *)command, "cd", 2) == 0)
+    { // change directory
+        printStr((char *)command, BIOS_LIGHT_BLUE);
+        if (memcmp("..", (void *)command + 3, 2) == 0)
+        {
+            if (depth == 0)
+            {
                 printStr("Udah paling ujung bos", BIOS_PINK);
                 return;
             }
@@ -53,21 +58,24 @@ void parseCommand(uint32_t command){
             printStr("Berhasil pindah ke", 0x2);
             printStr(listDir[depth], 0xF);
             return;
-        }else{
+        }
+        else
+        {
             // jika ingin masuk ke direktori
             struct FAT32DriverRequest request = {
                 .buf = &cl,
                 .parent_cluster_number = listCluster[depth],
-                .buffer_size = 0
-            };
+                .buffer_size = 0};
             memcpy(request.name, listDir[depth], 8);
             int32_t retcode = listCluster[depth];
             struct FAT32DirectoryTable table = {};
             request.buf = &table;
-            syscall(1, (uint32_t) &request, (uint32_t) &retcode, 0);
-            for (int i = 0 ; i < 64 ; i++){
-                if (memcmp(table.table[i].name, (char *) command + 3, 8) == 0){
-                    depth +=1;
+            syscall(1, (uint32_t)&request, (uint32_t)&retcode, 0);
+            for (int i = 0; i < 64; i++)
+            {
+                if (memcmp(table.table[i].name, (char *)command + 3, 8) == 0)
+                {
+                    depth += 1;
                     listCluster[depth] = table.table[i].cluster_low | (table.table[i].cluster_high << 16);
                     listDir[depth] = table.table[i].name;
                     return;
@@ -79,62 +87,117 @@ void parseCommand(uint32_t command){
             // listDir[2] = "cimeng";
         }
 
-    // ls
-    }else if(memcmp((char*) command, "ls", 2)==0){
-        printStr((char *) command, BIOS_LIGHT_BLUE);
+        // ls
+    }
+    else if (memcmp((char *)command, "ls", 2) == 0)
+    {
+        printStr((char *)command, BIOS_LIGHT_BLUE);
         struct FAT32DriverRequest request = {
             .buf = &cl,
-            .buffer_size = 0
-        };
-        if(!depth){
+            .buffer_size = 0};
+        if (!depth)
+        {
             request.parent_cluster_number = listCluster[depth];
-        }else{
-            request.parent_cluster_number = listCluster[depth-1];
-            uint32_t temp = listCluster[depth-1];
-            listCluster[depth-1] = temp;
+        }
+        else
+        {
+            request.parent_cluster_number = listCluster[depth - 1];
+            uint32_t temp = listCluster[depth - 1];
+            listCluster[depth - 1] = temp;
         }
         memcpy(request.name, listDir[depth], 8);
         int32_t retcode;
         struct FAT32DirectoryTable table = {};
         request.buf = &table;
-        syscall(1, (uint32_t) &request, (uint32_t) &retcode, 0);
-        switch (retcode){
-            case 0:
-                for (int i = 0 ; i < 64 ; i++){
-                    printStr(table.table[i].name, BIOS_LIGHT_GREEN);
-                    if(table.table[i].name[0] == '\0'){
-                        memcpy(listDir[depth], request.name, 8);
-                    }
+        syscall(1, (uint32_t)&request, (uint32_t)&retcode, 0);
+        switch (retcode)
+        {
+        case 0:
+            for (int i = 0; i < 64; i++)
+            {
+                printStr(table.table[i].name, BIOS_LIGHT_GREEN);
+                if (table.table[i].name[0] == '\0')
+                {
+                    memcpy(listDir[depth], request.name, 8);
                 }
-                break;
-            case 1:
-                printStr("Not a folder", BIOS_RED);
-                break;
-            case 2:
-                printStr("Not found", BIOS_RED);
-                break;
-            default:
-                printStr("Error", BIOS_RED);
-                break;
+            }
+            break;
+        case 1:
+            printStr("Not a folder", BIOS_RED);
+            break;
+        case 2:
+            printStr("Not found", BIOS_RED);
+            break;
+        default:
+            printStr("Error", BIOS_RED);
+            break;
         }
 
-    // mkdir
-    }else if(memcmp((char*) command, "mkdir", 5)==0){
-        printStr((char *) command, BIOS_LIGHT_BLUE);
-    // cat
-    }else if(memcmp((char*) command, "cat", 3)==0){
-        printStr((char *) command, BIOS_LIGHT_BLUE);
-    // cp 
-    }else if(memcmp((char*) command, "cp", 3)==0){
-        printStr((char *) command, BIOS_LIGHT_BLUE);
-    // rm 
-    }else if(memcmp((char*) command, "rm", 3)==0){
-        printStr((char *) command, BIOS_LIGHT_BLUE);
-    // mv 
-    }else if(memcmp((char*) command, "mv", 3)==0){
-        printStr((char *) command, BIOS_LIGHT_BLUE);
-    // find
-    }else{
+        // mkdir
+    }
+    else if (memcmp((char *)command, "mkdir", 5) == 0)
+    {
+        printStr((char *)command, BIOS_LIGHT_BLUE);
+        // cat
+    }
+    else if (memcmp((char *)command, "cat", 3) == 0)
+    {
+        struct FAT32DriverRequest request = {
+            .buf = &cl,
+            .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+            .buffer_size = 0,
+        };
+        request.buffer_size = 5 * CLUSTER_SIZE;
+        int nameLen = 0;
+        char *itr = (char *)command + 4;
+        for (int i = 0; i < strlen(itr); i++)
+        {
+            if (itr[i] == '.')
+            {
+                request.ext[0] = itr[i + 1];
+                request.ext[1] = itr[i + 2];
+                request.ext[2] = itr[i + 3];
+                break;
+            }
+            else
+            {
+                nameLen++;
+            }
+        }
+        memcpy(request.name, (void *)(command + 4), nameLen);
+        int32_t retcode;
+        syscall(0, (uint32_t)&request, (uint32_t)&retcode, 0);
+        if (retcode == 0)
+        {
+            printStr(request.buf, 0xF);
+            printStr("", 0xF);
+        }
+        else if (retcode == 1)
+            printStr("Bukan File ey", BIOS_RED);
+        else if (retcode == 2)
+            printStr("Gaada filenya", BIOS_RED);
+        else
+            printStr("Teuing error naon", BIOS_RED);
+
+        // cp
+    }
+    else if (memcmp((char *)command, "cp", 3) == 0)
+    {
+        printStr((char *)command, BIOS_LIGHT_BLUE);
+        // rm
+    }
+    else if (memcmp((char *)command, "rm", 3) == 0)
+    {
+        printStr((char *)command, BIOS_LIGHT_BLUE);
+        // mv
+    }
+    else if (memcmp((char *)command, "mv", 3) == 0)
+    {
+        printStr((char *)command, BIOS_LIGHT_BLUE);
+        // find
+    }
+    else
+    {
         printStr("Masukkin command yang bener dong", BIOS_LIGHT_BLUE);
     }
 }
@@ -154,7 +217,6 @@ int main(void)
     // if (retcode == 0)
     //     syscall(6, (uint32_t) "owo\n", 4, 0xF);
 
-
     syscall(7, 0, 0, 0);
     // char *terminal = "OperatingStess ";
 
@@ -166,7 +228,8 @@ int main(void)
         char buf[256];
         printStr("OperatingStess@User:~", BIOS_LIGHT_GREEN);
         int i = 0;
-        while(listDir[i]!= NULL){
+        while (listDir[i] != NULL)
+        {
             printStr("/", BIOS_LIGHT_GREEN);
             printStr(listDir[i], BIOS_LIGHT_GREEN);
             i++;
